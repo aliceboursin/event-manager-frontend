cbc7b8028d76a410a408eb56e2db2a3ce864106c
PepperMint5 <alice.boursin@dauphine.eu>
2024-06-22 22:49:08 +0200

    (Update) : login and signup : handle response from back, redicrection to envents page, errors fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,9 +3,11 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { AuthData } from '../../data/authdata';
 import { SessionStorageService } from '../../services/session.storage.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ToastService } from '../../services/toast.service';
 import { Router } from '@angular/router';
+import { SignUpComponent } from '../signup/signup.component';
+
 
 @Component({
   selector: 'login',
@@ -14,6 +16,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  isSubmitted: boolean = false;
+
     constructor(
       private fb: FormBuilder,
       private authService: AuthService, 
@@ -21,6 +25,8 @@ export class LoginComponent {
       public dialogRef: MatDialogRef<LoginComponent>,
       private toastService: ToastService,
       private router: Router,
+      public dialog: MatDialog,
+
     ){};
 
     form = this.fb.group({
@@ -48,23 +54,32 @@ export class LoginComponent {
     }
 
     onSubmit(){
+      this.isSubmitted = true;
+      if (this.form.valid) {
         const data: AuthData = {
-            username: this.form.get('username')?.value ?? '',
-            password: this.form.get('password')?.value ?? ''
-        }
-        this.authService.login(data).subscribe(
-            response => {
-              console.log('Login successful:', response);
-              const userId = response.userId;
-              this.sessionStorageService.setItem('userId', userId);
-              this.toastService.showToast("Welcome back !", "success");
-              this.goToEventsPage();
-            },
-            error => {
-              console.error('Login failed:', error);
-              this.toastService.showToast("Username or password incorrect", "error");
-            }
-          );
+          username: this.form.get('username')?.value ?? '',
+          password: this.form.get('password')?.value ?? ''
+      }
+      this.authService.login(data).subscribe(
+          response => {
+            console.log('Login successful:', response);
+            const userId = response.userId;
+            this.sessionStorageService.setItem('userId', userId);
+            this.toastService.showToast("Welcome back !", "success");
+            this.goToEventsPage();
+            this.closeDialog();
+          },
+          error => {
+            console.error('Login failed:', error);
+            this.toastService.showToast("Username or password incorrect", "error");
+          }
+        );
+        console.log(this.sessionStorageService.getItem('userId'));
+      }
+      else{
+        this.toastService.showToast("Please review your information", "error");
+    }
+        
     }
 
     
@@ -74,5 +89,13 @@ export class LoginComponent {
 
   goToEventsPage(){
     this.router.navigate(['/events']);
+  }
+
+  openSignUp(): void {
+    this.dialog.open(SignUpComponent, {
+      width: '50%',
+      position: { right: '0' },
+      panelClass: 'custom-dialog-container'
+    });
   }
 }
